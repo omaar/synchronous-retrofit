adc181ce2a95c94bb484460451c444184f010dc7
Omar Regino <omarjrz21@gmail.com>
2015-07-16 18:45:44 -0500

    Se agregaron encapsulan atributos

--- GiphyService.java
+++ GiphyService.java
@@ -1,3 +1,9 @@
+import java.awt.Desktop;
+import java.net.URI;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 import retrofit.RestAdapter;
 import retrofit.http.Header;
 import retrofit.http.GET;
@@ -8,6 +14,22 @@ import responses.*;
 public class GiphyService {
 	private RestAdapter adapter;
 	private Service service;
+
+	public RestAdapter getAdapter(){
+		return this.adapter;
+	}
+
+	public void setAdapter(RestAdapter adapter){
+		this.adapter = adapter;
+	}
+
+	public Service getService(){
+		return this.service;
+	}
+
+	public void setService(Service service){
+		this.service = service;
+	}
 
 	public interface Service{
 		@GET("/v1/gifs/search")
@@ -29,4 +51,30 @@ public class GiphyService {
 	public GiphyResponse searchGifs(String term){
 		return service.search(Constants.GIPHY_KEY, term);
 	}
+
+	public void searchGifs(String term, int numImages){
+
+		GiphyResponse gifs = searchGifs(term);
+		int i = 0;
+
+		for (Gif gif : gifs.getData()) {
+			i++;
+			if(i <= numImages){
+				openInBrowser(gif.images.fixed_height.url);
+			}
+		}
+	}
+
+	public void openInBrowser(String url){
+		if(Desktop.isDesktopSupported()){
+			try {
+				Desktop.getDesktop().browse(new URI(url));
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+					} catch(IOException e){
+						e.printStackTrace();
+						}
+						}
+	}
+
 }
--- Main.java
+++ Main.java
@@ -1,42 +1,18 @@
 import retrofit.Callback;
 import responses.*;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-import java.awt.Desktop;
-import java.net.URI;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.Scanner;
 
 public class Main{
+
 	public static void main(String[] args) {
-			Main.searchGifs("hackerz", 1);
-	}
 
-	public static void searchGifs(String term, int numImages){
 		GiphyService service = new GiphyService();
+		Scanner readC = new Scanner(System.in);
 
-		GiphyResponse gifs = service.searchGifs(term);
-		int i = 0;
-
-		for (Gif gif : gifs.getData()) {
-			i++;
-			if(i <= numImages){
-				Main.openInBrowser(gif.images.fixed_height.url);
-			}
-		}
+		System.out.println("Busca un GIF: ");
+		String term = readC.nextLine();
+		System.out.println("Numero: ");
+			service.searchGifs(term, readC.nextInt());
 	}
 
-	public static void openInBrowser(String url){
-		if(Desktop.isDesktopSupported()){
-			try {
-                Desktop.getDesktop().browse(new URI(url));
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            } catch(IOException e){
-                e.printStackTrace();
-            }
-			
-		}
-	}
 }
